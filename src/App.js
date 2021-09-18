f5258c4e59e922e1f6dabc3636a225ddc9e3bc82
OrionXD <sajad.laskar1@gmail.com>
2021-09-18 15:14:30 +0530

    added nav-bar.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Home from './Home';
+import Home from "./Home";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
